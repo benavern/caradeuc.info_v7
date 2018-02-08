8862852cc33274304f85e16582130cdf6e4d432b
Benjamin Caradeuc <bencrdc@gmail.com>
2018-02-08 19:48:33 +0100

    Add settings for styles & stuff

--- build/utils.js
+++ build/utils.js
@@ -59,8 +59,8 @@ exports.cssLoaders = function (options) {
     css: generateLoaders(),
     postcss: generateLoaders(),
     less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    sass: generateLoaders('sass', { indentedSyntax: true, data: '@import "./src/styles/settings"' }),
+    scss: generateLoaders('sass', { data: '@import "./src/styles/settings"' }),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
--- src/components/ZButton.vue
+++ src/components/ZButton.vue
@@ -13,12 +13,19 @@ export default {
 <style lang="sass" scoped>
 button
   display: inline-block
-  background: #428f68
+  background: $green
   height: 2.5em
   line-height: 2.5em
-  color: #fff
+  color: $white
   padding: 0 1.5em
   border: none
   border-radius: 2.5em
   outline: none
+  box-shadow: 0 0 .8em rgba($black, .5)
+  transition: box-shadow .3s
+
+  &:hover
+    box-shadow: 0 0 .8em rgba($black, .8)
+    cursor: pointer
+
 </style>
