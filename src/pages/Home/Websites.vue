7786ab57ee3a1ad362dfb11a9a8073c00af1aff6
Benjamin Caradeuc <bencrdc@gmail.com>
2018-03-16 00:24:44 +0100

    Add Websites section

---
+++ src/pages/Home/Websites.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="websites">
+    <website-item v-for="(website, i) in websites"
+                  :key="i"
+                  :name="website.name"
+                  :description="website.description"
+                  :url="website.url"
+                  :img="website.img"
+                  v-if="website.display"/>
+  </section>
+</template>
+
+<script>
+import WebsitesData from './websitesData'
+import WebsiteItem from '@/components/WebsiteItem'
+
+export default {
+  name: 'Websites',
+  components: { WebsiteItem },
+  data () {
+    return {
+      websites: WebsitesData
+    }
+  }
+}
+</script>
